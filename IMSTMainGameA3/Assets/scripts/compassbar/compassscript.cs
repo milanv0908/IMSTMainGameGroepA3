b300cb94f3d65826794a8eb9b8ae30a4cdadac82
Quintus <quintenvangils@outlook.com>
2023-10-25 12:07:33 +0200

    compassbarv3

--- IMSTMainGameA3/Assets/scripts/compassbar/compassscript.cs
+++ IMSTMainGameA3/Assets/scripts/compassbar/compassscript.cs
@@ -12,26 +12,85 @@ public class compassscript : MonoBehaviour
 
     float compassUnit;
 
-    private void Start() {
-        compassUnit = compassImage.rectTreansform.rect.width / 360f; 
+    public QuestMarker Frank;
+
+    private void Start()
+    {
+        compassUnit = compassImage.rectTransform.rect.width / 360f;
+        AddQuestMarker(Frank);
     }
 
-    private void Update() {
-        compassImage.uvRect = new Rect (player.localEulerAngles.y / 360f, 0f, 1f, 1f);
+    private void Update()
+    {
+        compassImage.uvRect = new Rect(player.localEulerAngles.y / 360f, 0f, 1f, 1f);
+
+        foreach (QuestMarker marker in questMarkers)
+        {
+            marker.image.rectTransform.anchoredPosition = GetPosOnCompass(marker);
+        }
     }
-    public void AddQuestMarker (QuestMarker marker) {
+
+    public void AddQuestMarker(QuestMarker marker)
+    {
         GameObject newMarker = Instantiate(iconPrefab, compassImage.transform);
-        marker.image = newMarker.GetComponent<compassImage>();
+        marker.image = newMarker.GetComponent<Image>();
         marker.image.sprite = marker.icon;
-        questMarkers.Add(marker); 
+        questMarkers.Add(marker);
     }
 
-    Vector2 GetPosOnCompass (QuestMarker marker) {
-        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.Z)
-        Vector2 playerFwd = new Vector2(player.transform.forward.x, player.transform.forward.Z)
+    Vector2 GetPosOnCompass(QuestMarker marker)
+    {
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.z); // Changed 'Z' to lowercase 'z'
+        Vector2 playerFwd = new Vector2(player.transform.forward.x, player.transform.forward.z); // Changed 'Z' to lowercase 'z'
 
         float angle = Vector2.SignedAngle(marker.position - playerPos, playerFwd);
 
-        return new Vector2(compassUnit * angle, 0f)
+        return new Vector2(compassUnit * angle, 0f);
     }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class compassscript : MonoBehaviour
+// {
+//     public GameObject iconPrefab;
+//     List<QuestMarker> questMarkers = new List<QuestMarker>();
+//     public RawImage compassImage;
+//     public Transform player;
+
+//     float compassUnit;
+
+//     public QuestMarker Frank;
+
+//     private void Start() {
+//         compassUnit = compassImage.rectTransform.rect.width / 360f;
+//         AddQuestMarker(Frank);
+//     }
+
+//     private void Update() {
+//         compassImage.uvRect = new Rect (player.localEulerAngles.y / 360f, 0f, 1f, 1f);
+
+//         foreach (QuestMarker marker in questMarkers) {
+//             marker.image.rectTransform.anchoredPosition = GetPosOnCompass(marker);
+
+//         }
+//     }
+//     public void AddQuestMarker (QuestMarker marker) {
+//         GameObject newMarker = Instantiate(iconPrefab, compassImage.transform);
+//         marker.image = newMarker.GetComponent<compassImage>();
+//         marker.image.sprite = marker.icon;
+//         questMarkers.Add(marker); 
+//     }
+
+//     Vector2 GetPosOnCompass (QuestMarker marker) {
+//         Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.Z);
+//         Vector2 playerFwd = new Vector2(player.transform.forward.x, player.transform.forward.Z);
+
+//         float angle = Vector2.SignedAngle(marker.position - playerPos, playerFwd);
+
+//         return new Vector2(compassUnit * angle, 0f);
+//     }
+// }
