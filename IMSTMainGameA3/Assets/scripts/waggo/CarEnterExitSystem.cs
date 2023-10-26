3e35a3aff4ebfa24e42b976cbe886516fabb0d97
Quintus <quintenvangils@outlook.com>
2023-10-26 11:47:57 +0200

    stepv3

--- IMSTMainGameA3/Assets/scripts/waggo/CarEnterExitSystem.cs
+++ IMSTMainGameA3/Assets/scripts/waggo/CarEnterExitSystem.cs
@@ -1,19 +1,79 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+
+// public class CarEnterExitSystem : MonoBehaviour
+// {
+//     public MonoBehaviour CarController2;
+//     public GameObject Player;
+//     bool Candrive;
+
+//     void Start()
+//     {
+//         CarController2.enabled = false;
+//     }
+
+//     void Update()
+//     {
+
+//     }
+
+//     void OnTriggerStay(Collider col)
+//     {
+//         if (col.gameObject.tag == "Player")
+//         {
+//             Candrive = true;
+//         }
+//     }
+
+//     void OnTriggerExit(collider col)
+//     {
+//         if (col.gameObject.tag == "Player")
+//         {
+//             Candrive = false;
+//         }
+//     }
+
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class CarEnterExitSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public MonoBehaviour CarController2;
+    public Transform Player;
+    public Transform Car;
+    bool CanDrive;
+
     void Start()
     {
-        
+        CarController2.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+    if (Input.GetKeyDown(KeyCode.F) && CanDrive) {
+            player.transform.SetParent(Car);
+            Player.gameObject.SetActive(false);
+    }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            CanDrive = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            CanDrive = false;
+        }
     }
 }
