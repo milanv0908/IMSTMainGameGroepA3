9171c4b17da52e9e17015bf0e6f5a6ebb0dac1e0
Quintus <quintenvangils@outlook.com>
2023-10-27 17:06:26 +0200

    deskinrichten1

--- IMSTMainGameA3/Assets/scripts/Spelerscripts/objectivescript1.cs
+++ IMSTMainGameA3/Assets/scripts/Spelerscripts/objectivescript1.cs
@@ -5,16 +5,23 @@ using TMPro;
 
 public class objectivescript1 : MonoBehaviour
 {
+    AudioSource audiosource;
     public TextMeshProUGUI text;
     public koffiezetapparaat koffiedrinken;
     public koffiemok koffiemok;
+    public AudioClip objectivegeluid;
+    public spelertelefoon spelerbool;
     private bool textchanged1 = false;
     private bool textchanged2 = false;
+
+    private bool textchanged3 = false;
+
 
     void Start()
     {
         text.enabled = true;
         text.text = "Objective: Set a cup of Coffee";
+        audiosource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -23,6 +30,8 @@ public class objectivescript1 : MonoBehaviour
         {
             text.text = "Objective: Drink the cup of coffee";
             textchanged1 = true;
+            audiosource.PlayOneShot(objectivegeluid);
+
         }
 
         if (koffiemok.telefoonfrank == true && !textchanged2)
@@ -30,5 +39,14 @@ public class objectivescript1 : MonoBehaviour
             text.text = "Objective: Answer the phone and listen to the caller";
             textchanged2 = true;
         }
+
+            if (spelerbool.DeurOpen == true && !textchanged3)
+        {
+            text.text = "Leave the appartment and visit william";
+            textchanged3 = true;
+            audiosource.PlayOneShot(objectivegeluid);
+        }
+
+        
     }
 }
