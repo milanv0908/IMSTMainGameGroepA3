6eed04b958956a0dfd9f9258e0f2334b94ce4a4f
milanv0908 <milanverduin2005@gmail.com>
2023-10-30 15:21:13 +0100

    nomorecrosshair

--- IMSTMainGameA3/Assets/scripts/appartement/deurklink.cs
+++ IMSTMainGameA3/Assets/scripts/appartement/deurklink.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 
 public class deurklink : MonoBehaviour
 {
+    public GameObject UIUpdate; // Het gameobject met de Image-component
     Animator animator;
     public bool isActivated;
+    public Transform player; // Spelerreferentie
+
+    private float activationDistance = 5.0f;
+
     void Start()
     {
-    animator = GetComponent<Animator>();
-    isActivated = false;
+        animator = GetComponent<Animator>();
+        isActivated = false;
+        UIUpdate.SetActive(false);
     }
-public void activate() {
-animator.SetTrigger("activate");
+
+   void Update()
+{
+    if (player != null)
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= activationDistance)
+        {
+            UIUpdate.SetActive(true);
+        }
+        else
+        {
+            UIUpdate.SetActive(false);
+        }
+
+        // Bijwerk de rotatie van het UIUpdate-object om met de speler mee te draaien
+        UIUpdate.transform.rotation = Quaternion.Euler(-90, player.rotation.eulerAngles.y, 0);
+        //  UIUpdate.transform.rotation = Quaternion.Euler(270, player.rotation.eulerAngles.z, 0);
+        
+        // Reset de schaal naar positieve waarden
+        // UIUpdate.transform.localScale = new Vector3(1, 1, 1);
+    }
+}
+
+    public void activate()
+    {
+        animator.SetTrigger("activate");
         isActivated = true;
         StartCoroutine(openDeur());
-}
+    }
 
-IEnumerator openDeur() {
+    IEnumerator openDeur()
+    {
         yield return new WaitForSeconds(0.1f);
         isActivated = false;
-
-}
+    }
 }
