ea0f5cf1cd4cd0b4ee30bd3f5378815e832179d0
milanv0908 <milanverduin2005@gmail.com>
2023-11-09 00:30:00 +0100

    fuk avans

--- IMSTMainGameA3/Assets/scripts/buiten/objectives.cs
+++ IMSTMainGameA3/Assets/scripts/buiten/objectives.cs
@@ -10,10 +10,15 @@ public class objectives : MonoBehaviour
     public streetevent streetevent;
     public mondmaskerdispenser facemask;
      public suzanne activate;
+
+         AudioSource audiosource;
+    public AudioClip objectivegeluid;
     void Start()
     {
+        audiosource = GetComponent<AudioSource>();
         text.enabled = true;
         text.text = "Objective: Walk to the busstation.";
+        audiosource.PlayOneShot(objectivegeluid);
     }
 
     // Update is called once per frame
@@ -21,14 +26,17 @@ public class objectives : MonoBehaviour
     {
        if (streetevent.hasinteracted == true) {
             text.text = "Objective: Maybe somebody in park can tell you where to find a facemask.";
+            audiosource.PlayOneShot(objectivegeluid);
        }
 
        if (activate.Suzanne == true) {
                text.text = "Objective: Buy a facemask at the dispenser.";
+            //    audiosource.PlayOneShot(objectivegeluid);
        }
 
        if (facemask.objectivebus == true) {
             text.text = "Objective: Go to the busstation and wait for your ride";
+            // audiosource.PlayOneShot(objectivegeluid);
        }
     }
 }
