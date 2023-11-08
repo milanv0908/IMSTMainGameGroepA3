688c0930625d5fe48809e03dafe609d022631166
milanv0908 <milanverduin2005@gmail.com>
2023-11-08 11:57:44 +0100

    suzanne

---
+++ IMSTMainGameA3/Assets/scripts/suzanne/GertInteract.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GertInteract : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool GertTalked = false;
+public void suzanneActive(){
+    GertTalked = true;
+}
+}
