689455f55d969888995ca5ef8f40ba73b1a7fb7b
Quintus <quintenvangils@outlook.com>
2023-11-08 13:28:58 +0100

    UI profielfoto

--- IMSTMainGameA3/Assets/scripts/buiten/streetevent.cs
+++ IMSTMainGameA3/Assets/scripts/buiten/streetevent.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class streetevent : MonoBehaviour
 {
     public bool hasinteracted = false;
+    public Image masker;
+    public Image geenmasker;
 
     void OnTriggerEnter()
     {
         hasinteracted = true;
         StartCoroutine(bloep());
+        masker.enabled = false;
+        geenmasker.enabled = true;
         
     }
 
