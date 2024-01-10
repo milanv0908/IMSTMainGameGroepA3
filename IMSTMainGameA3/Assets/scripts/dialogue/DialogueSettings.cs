b14a65f06e45294781c3e97586c6347fb471f690
milanv0908 <milanverduin2005@gmail.com>
2024-01-10 16:07:25 +0100

    ik ben slim

--- IMSTMainGameA3/Assets/scripts/dialogue/DialogueSettings.cs
+++ IMSTMainGameA3/Assets/scripts/dialogue/DialogueSettings.cs
@@ -1,12 +1,3 @@
-// op het object voeg als component het NPCConversationscript
-//voeg dit script toe
-//sleep dit in je object interactionscript en kies als functie interactietest1
-//voeg de speler/camera etc. script toe om te disabelen in je inspector.
-//bij conversation voeg het object toe waarmee je wilt praten (deze is hetzelfde als waar dit script op zit)
-//ga naar windows dialogue editor en edit de dialoog. vergeet niet om de audio op 1 te zetten
-//wil je nieuwe dingen doe dat in je editor met rechterklik dan krijg je de opties
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,12 +5,11 @@ using DialogueEditor;
 
 public class dialoguesettings : MonoBehaviour
 {
-
-     public Camerabob Camerabob; // Voeg een referentie naar het bewegingsscript van de speler toe.
+    public Camerabob Camerabob;
     public NPCConversation Conversation;
     public speler player;
-    public ObjectInteraction scriptToDisable; // Voeg een referentie naar het script dat je wilt uitschakelen toe.
-    public PlayerMove playerMovement; // Voeg een referentie naar het bewegingsscript van de speler toe.
+    public ObjectInteraction scriptToDisable;
+    public PlayerMove playerMovement;
 
     private void Update()
     {
@@ -27,13 +17,10 @@ public class dialoguesettings : MonoBehaviour
         {
             if (ConversationManager.Instance.IsConversationActive)
             {
-                // Schakel het script uit wanneer de conversatie actief is
                 scriptToDisable.enabled = false;
-                // Schakel de beweging van de speler uit
                 playerMovement.enabled = false;
                 Camerabob.enabled = false;
 
-                // Voer de rest van je dialooglogica uit
                 if (Input.GetKeyDown(KeyCode.UpArrow))
                     ConversationManager.Instance.SelectPreviousOption();
                 else if (Input.GetKeyDown(KeyCode.DownArrow))
@@ -41,18 +28,13 @@ public class dialoguesettings : MonoBehaviour
                 else if (Input.GetKeyDown(KeyCode.E))
                     ConversationManager.Instance.PressSelectedOption();
 
-                // Controleer of de bool "kies2" true is
-        
+
             }
             else
             {
-                // Zorg ervoor dat het script weer wordt ingeschakeld wanneer de conversatie niet actief is
                 scriptToDisable.enabled = true;
-                // Schakel de beweging van de speler weer in
                 playerMovement.enabled = true;
                 Camerabob.enabled = true;
-
-                // Voer andere logica uit wanneer de conversatie niet actief is
             }
         }
     }
