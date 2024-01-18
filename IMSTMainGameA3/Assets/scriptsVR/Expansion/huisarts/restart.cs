f462ca50973f260e16199fa82f374c5634a3e78e
Quintus <quintenvangils@outlook.com>
2024-01-18 13:55:27 +0100

    hubhubhub

--- IMSTMainGameA3/Assets/scriptsVR/Expansion/huisarts/restart.cs
+++ IMSTMainGameA3/Assets/scriptsVR/Expansion/huisarts/restart.cs
@@ -1,11 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class restart : MonoBehaviour
 {
-public void restar() {
-    SceneManager.LoadScene("mainmenu");
-}
+    public TV tv;
+    public Image image;
+
+void Start() {
+        image.enabled = false;
+    }
+    public void restar() {
+        if (tv.leave == false) {
+            SceneManager.LoadScene("mainmenu");
+            
+        }
+    }
+
+    void Update() {
+        if(tv.leave == false) {
+            image.enabled = true;
+            Debug.Log("hop");
+        }
+    }
+
+
 }
